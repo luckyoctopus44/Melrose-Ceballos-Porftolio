7bd7fdf4e66d6d79e70fbc6773bac81aceef8c03
Melrose Ceballos <melroseceballos@Melroses-MBP.lan>
2023-02-22 12:43:38 -0800

    About Me functionalities done

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-//REDIRECT BUTTON TO ABOUT ME PAGE
-
+// DIRECT BUTTON TO ABOUT ME PAGE
 function exploreClick () {
     location.href = "/about-me.html"
+}
+
+// DIRECT TO HOME
+function directHome(){
+    location.href = "/home-page.html"
+}
+// DIRECT TO ABOUT ME
+function directAboutMe() { 
+    location.href="/about-me.html"
+}
+// DIRECT PROJECTS
+function directProjects(){
+    location.href="/projects.html"
+}
+//DIRECT CONTACT ME
+function directContactMe(){
+    location.href="/contact-me.html"
 }
